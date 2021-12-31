14ab3ab6d0aacf4e01c9d7ff80330fa7295f0ae2
Liszt21 <1832666492@qq.com>
2021-12-31 10:46:09 +0800

    feat(config): load & save

--- src/main.lisp
+++ src/main.lisp
@@ -4,15 +4,43 @@
 
 (in-package ust)
 
-(defparameter *commands*
-  (list
-   (cons 'lisp "ros")
-   (cons 'py "python")
-   (cons 'js "node")
-   (cons 'ts "node")
-   #-os-windows (cons 'sh "sh")
-   #+os-windows (cons 'cmd "cmd")
-   #+os-windows (cons 'ps1 "powershell")))
+(defun load-file (path &optional (default '()))
+  (if (probe-file path)
+      (with-open-file (in path)
+        (with-standard-io-syntax
+          (format t "Load config from ~A~%" path)
+          (read in)))
+      default))
+
+(defun load-config ()
+  (load-file
+    #p"~/.config/ust/config"
+    (list (list 'commands
+                (cons 'lisp "ros")
+                (cons 'py "python")
+                (cons 'js "node")
+                (cons 'ts "node")
+                #-os-windows (cons 'sh "sh")
+                #+os-windows (cons 'cmd "cmd")
+                #+os-windows (cons 'ps1 "powershell"))
+          (cons 'repos
+            (list #p"~/.dotfiles/scripts/"
+                  #p"~/.config/ust/scripts/"
+                  #p"~/.scripts/")))))
+
+(defun load-cache () (load-file #p"~/.config/ust/cache" '((scripts))))
+
+(defun save-file (content path)
+  (uiop:ensure-all-directories-exist (list path))
+  (with-open-file (out path :direction :output :if-exists :supersede)
+    (with-standard-io-syntax
+      (print content out))))
+
+(defun save-config () (save-file *config* #p"~/.config/ust/config"))
+(defun save-cache () (save-file *cache* #p"~/.config/ust/cache"))
+
+(defparameter *config* (load-config))
+(defparameter *cache* (load-cache))
 
 (defun shell (&rest cmds)
   (let ((command (str:join ";" cmds)))
@@ -28,32 +56,29 @@
        :ignore-error-status t
        :error-output :interactive)))))
 
-(defun detect-repository ()
-  (loop for path-str in (append
-                         (list #p"~/.dotfiles/scripts"
-                               #p"~/.config/ust/scripts"
-                               #p"~/.scripts")
-                         (uiop:getenv-pathnames "UST_SCRIPT"))
-        for path = (probe-file path-str)
-        when path
-        collect path))
+(defun update-cache (key value)
+  (setf (cdr (assoc key *cache*)) value))
 
-(defparameter *repository* (detect-repository))
-
-(defun get-scripts (&optional (repos *repository*))
-  (apply #'append
-           (loop for repo in repos
-                 collect (loop for file in (uiop:directory-files repo)
-                               when (member (intern (string-upcase (pathname-type file))) *commands* :key #'car)
-                               collect (cons (pathname-name file) file)))))
-
-(defparameter *scripts* (get-scripts))
+(defun scan-scripts ()
+  (let* ((repos (cdr (assoc 'repos *config*)))
+         (commands (cdr (assoc 'commands *config*)))
+         (scripts
+          (apply #'append
+                  (loop for path in repos
+                        for repo = (probe-file (uiop:ensure-directory-pathname path))
+                        when repo
+                        do (format t "Scaning scripts in ~A...~%" path)
+                        when repo
+                        collect (loop for file in (uiop:directory-files repo)
+                                      when (assoc (intern (string-upcase (pathname-type file))) commands)
+                                      collect (cons (pathname-name file) file))))))
+    (update-cache 'scripts (or scripts '()))))
 
 (defun get-script-path (name)
   (let* ((tmp (str:split "." name))
          (name (car tmp))
          (type (cadr tmp)))
-    (cdar (member (cons name type) *scripts*
+    (cdar (member (cons name type) (cdr (assoc 'scripts *cache*))
                   :test (lambda (a b)
                           (and (equal (car a) (car b))
                                (or (not (cdr a))
@@ -61,7 +86,7 @@
 
 (defun run-script (script &rest args)
   (let* ((type (intern (string-upcase (pathname-type script))))
-         (cmd (cdar (member type *commands* :key #'car))))
+         (cmd (cdar (member type (cdr (assoc 'commands *config*)) :key #'car))))
     (format t "Eval script ~A~%" script)
     (if cmd
         (shell (format nil "~A ~A~{ ~A~}" cmd script args))
@@ -70,15 +95,15 @@
 (defun default (&rest arguments)
   (let ((cmd (car arguments))
         (args (cdr arguments)))
-    (if cmd
+    (when cmd
       (let ((script (get-script-path cmd)))
         (if script
             (apply #'run-script (cons script args))
-            (format t "Script ~A not founded~%" cmd)))
-      (format t "ss"))))
+            (format t "Script ~A not founded~%" cmd))))))
 
 (defun list-script ()
-  (format t "Scripts:~%~{  ~A~%~}~%" *scripts*))
+  (scan-scripts)
+  (format t "Scripts:~%~{  ~A~%~}~%" (cdr (assoc 'scripts *cache*))))
 
 (defun cat-script (script)
   (let ((script (get-script-path script)))
@@ -88,12 +113,20 @@
   (dolist (script scripts)
     (default script action)))
 
+(defun check (&rest args)
+  (when (and (not (eq (intern "LIST" 'command) (car args)))
+             (not (cdr (assoc 'scripts *cache*))))
+    (format t "Empty scripts, scaning...~%")
+    (scan-scripts)))
+
 (clish:defcli cli
   (:default #'default)
-  (list #'list-script)
+  (:pre #'check)
+  (:post (lambda (&rest args) (save-cache)))
   (cat #'cat-script)
-  (install (lambda (&rest scripts) (apply #'dispatch (cons "install" scripts))))
+  (list #'list-script)
   (remove (lambda (&rest scripts) (apply #'dispatch (cons "remove" scripts))))
-  (update (lambda (&rest scripts) (apply #'dispatch (cons "update" scripts)))))
+  (update (lambda (&rest scripts) (apply #'dispatch (cons "update" scripts))))
+  (install (lambda (&rest scripts) (apply #'dispatch (cons "install" scripts)))))
 
 
